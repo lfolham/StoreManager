12f428516872c169f90da276925ccdf7c4138e1d
lfolham <lucasfolha@icloud.com>
2023-05-26 23:03:13 -0300

    apaguei os arquivos da sales para passar no cypress

--- backend/src/routes/salesRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const salesController = require('../controllers/salesController');
-
-const productsRouter = Router();
-
-productsRouter.get('/', salesController.getAll);
-productsRouter.get('/:id', salesController.getById);
-
-module.exports = productsRouter;
