473d8bb4c36f19e90957a579b319a63e3167d9b4
lfolham <lucasfolha@icloud.com>
2023-05-28 16:34:32 -0300

    começando testes

--- backend/src/models/salesModel.js
+++ backend/src/models/salesModel.js
@@ -1,19 +1,26 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const [result] = await connection
-  .execute(`SELECT sale_id, product_id, quantity, sales.date FROM sales 
-      INNER JOIN sales_products ON sales.id = sale_id
-      ORDER BY sale_id, product_id;`);
-  return result;
+  const [rows] = await connection.execute(`
+    SELECT sale_id, sales.date, product_id, quantity
+    FROM sales 
+    INNER JOIN sales_products ON sales.id = sale_id
+    ORDER BY sales.date, product_id, quantity;
+  `);
+  return rows;
 };
 
 const getById = async (id) => {
-  const [[result]] = await connection
-  .execute(`SELECT product_id, quantity, sales.date FROM sales 
-      INNER JOIN sales_products ON sales.id = sale_id AND sale_id = ?
-      ORDER BY sale_id, product_id;`, [id]);
-  return result;
+  const [rows] = await connection.execute(`
+    SELECT sales.date, product_id, quantity
+    FROM sales 
+    INNER JOIN sales_products ON sales.id = sale_id AND sale_id = ?
+    ORDER BY sales.date, product_id, quantity;
+  `, [id]);
+  if (rows.length === 0) {
+    return null;
+  }
+  return rows[0];
 };
 
 module.exports = {
