e65c92539cb1639f3dac7125d36ca8e9b47a4c7d
lfolham <lucasfolha@icloud.com>
2023-05-28 18:42:10 -0300

    task 2 done

--- backend/tests/unit/services/sales.service.test.js
+++ backend/tests/unit/services/sales.service.test.js
@@ -5,28 +5,33 @@ const { expect } = chai;
 
 const salesService = require('../../../src/services/salesService');
 const salesModel = require('../../../src/models/salesModel');
-const { salesProductss } = require('../controllers/sales.controller.test');
+const { salesProductss } = require('./mock/sales.mock');
 
 describe('Test SALES na camada Service', function () {
-  it('Test se retorna a lista de sales', async function () {
-    sinon.stub(salesModel, 'getAll').resolves(salesProductss);
-    const result = await salesService.getAll();
-    expect(result).to.be.deep.equal(salesProductss);
+  describe('teste', function () {
+    it('Test se retorna a lista de sales', async function () {
+      sinon.stub(salesModel, 'getAll').resolves(salesProductss);
+      const result = await salesService.getAll();
+      expect(result).to.be.deep.equal(salesProductss);
+    });
   });
 
   describe('Test getById()', function () {
     it('ID inválido', async function () {
       sinon.stub(salesModel, 'getById').resolves(null);
       const result = await salesService.getById(999);
-      expect(result).to.be.deep.equal({ type: 'SALE_NOT_FOUND', message: 'Sale not found' });
+
+      expect(result).to.be.deep.equal({ type: 404, message: 'Sale not found' });
     });
-  });
 
     it('ID válido', async function () {
-      sinon.stub(salesModel, 'getById').resolves(salesProductss[1]);
-      const result = await salesService.getById(1);
-      expect(result.message).to.be.deep.equal(salesProductss[1]);
+      sinon.stub(salesModel, 'getById').resolves(salesProductss[2]);
+      const result = await salesService.getById(2);
+      
+      expect(result.type).to.be.equal(null);
+      expect(result.message).to.be.deep.equal(salesProductss[2]);
     });
+  });
 
   afterEach(function () {
     sinon.restore();
